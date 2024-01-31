3d425a2fd420cba4d9b76c53ab400dd8f8798d56
elodiegermani1 <elodie.germani@irisa.fr>
2024-01-31 13:51:25 +0100

    Refact repo + launch on 4 classes 30 groups

--- src/jeanzay_launcher.sh
+++ src/jeanzay_launcher.sh
@@ -25,8 +25,8 @@ source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh
 
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/classifier_conditional_ddpm/src/main.py \
    --mode train --dataset dataset_rh_4class-30grp-jeanzay --labels pipelines \
-   --batch_size 8 --data_dir ../data --n_classes 4 \
-   --n_epoch 500 --lrate 1e-5 --sample_dir ../samples-4classes-30grp --save_dir ../models-4classes-30grp
+   --batch_size 8 --data_dir data --n_classes 4 \
+   --n_epoch 500 --lrate 1e-5 --sample_dir samples-4classes-30grp --save_dir models-4classes-30grp
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/conditional_ddpm/main.py \
 #    --mode train --dataset dataset_rh-jeanzay --labels pipelines \
